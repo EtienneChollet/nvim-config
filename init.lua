f115c69c624ecc7a86220f63533ca6e94998e733
Etienne Chollet <etiennepchollet@gmail.com>
2025-11-02 16:04:04 -0500

    [feat] add edit neovim command

--- init.lua
+++ init.lua
@@ -518,6 +518,11 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
       vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
+      vim.keymap.set('n', '<leader>en', function()
+        require('telescope.builtin').find_files {
+            cwd = vim.fn.stdpath("config")
+        }
+ end)
 
       -- Slightly advanced example of overriding default behavior and theme
       vim.keymap.set('n', '<leader>/', function()
