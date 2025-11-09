91c4cd9cffce8777f2540aab20c8501047fd45c0
Etienne Chollet <etiennepchollet@gmail.com>
2025-11-09 11:10:02 -0500

    [refactor] add harpoon

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -56,12 +56,53 @@ return {
       -- Git keymaps
       vim.keymap.set('n', '<leader>gs', '<cmd>Git<CR>', { desc = '[G]it [S]tatus' })
       vim.keymap.set('n', '<leader>gd', '<cmd>Gdiffsplit<CR>', { desc = '[G]it [D]iff split' })
-      vim.keymap.set('n', '<leader>gD', '<cmd>Gdiffsplit HEAD<CR>', { desc = '[G]it [D]iff vs HEAD' })
+      vim.keymap.set('n', '<leader>gD', '<cmd>Gvdiffsplit! HEAD<CR>', { desc = '[G]it [D]iff vs HEAD' })
       vim.keymap.set('n', '<leader>gc', '<cmd>Git commit<CR>', { desc = '[G]it [C]ommit' })
       vim.keymap.set('n', '<leader>gp', '<cmd>Git push<CR>', { desc = '[G]it [P]ush' })
       vim.keymap.set('n', '<leader>gl', '<cmd>Git pull<CR>', { desc = '[G]it Pu[l]l' })
       vim.keymap.set('n', '<leader>gb', '<cmd>Git blame<CR>', { desc = '[G]it [B]lame' })
       vim.keymap.set('n', '<leader>gL', '<cmd>Git log<CR>', { desc = '[G]it [L]og' })
+    end,
+  },
+  {
+    'ThePrimeagen/harpoon',
+    branch = 'harpoon2',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    config = function()
+      local harpoon = require('harpoon')
+      harpoon:setup()
+
+      -- Add current file to harpoon
+      vim.keymap.set('n', '<leader>a', function()
+        harpoon:list():add()
+      end, { desc = 'Harpoon [A]dd file' })
+
+      -- Toggle harpoon quick menu
+      vim.keymap.set('n', '<C-e>', function()
+        harpoon.ui:toggle_quick_menu(harpoon:list())
+      end, { desc = 'Harpoon menu' })
+
+      -- Jump to marked files
+      vim.keymap.set('n', '<leader>1', function()
+        harpoon:list():select(1)
+      end, { desc = 'Harpoon file 1' })
+      vim.keymap.set('n', '<leader>2', function()
+        harpoon:list():select(2)
+      end, { desc = 'Harpoon file 2' })
+      vim.keymap.set('n', '<leader>3', function()
+        harpoon:list():select(3)
+      end, { desc = 'Harpoon file 3' })
+      vim.keymap.set('n', '<leader>4', function()
+        harpoon:list():select(4)
+      end, { desc = 'Harpoon file 4' })
+
+      -- Cycle through marked files
+      vim.keymap.set('n', '<C-n>', function()
+        harpoon:list():next()
+      end, { desc = 'Harpoon next' })
+      vim.keymap.set('n', '<C-p>', function()
+        harpoon:list():prev()
+      end, { desc = 'Harpoon prev' })
     end,
   },
 }
