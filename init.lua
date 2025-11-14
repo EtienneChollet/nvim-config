590fc4a41175a21f330fb9baea16f1804494c21e
Etienne Chollet <etiennepchollet@gmail.com>
2025-11-14 12:03:39 -0500

    [feat ;5;99~;5;99~;5;99~;5;59~:;5;99~;5;99~;5;99~;5;99~;5;99~;5;99~
    exit

--- init.lua
+++ init.lua
@@ -118,7 +118,7 @@ vim.o.undodir = nvim_local_dir .. '/undo//'
 vim.env.XDG_STATE_HOME = nvim_local_dir .. '/state'
 
 -- Set to true if you have a Nerd Font installed and selected in the terminal
-vim.g.have_nerd_font = false
+vim.g.have_nerd_font = true
 
 -- [[ Setting options ]]
 -- See `:help vim.o`
@@ -502,6 +502,15 @@ vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagn
 -- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
 -- or just use <C-\><C-n> to exit terminal mode
 vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+
+-- Create a custom command for opening a terminal at 30% height
+vim.api.nvim_create_user_command('Term30', function()
+  local height = math.floor(vim.o.lines * 0.3)
+  vim.cmd('belowright ' .. height .. 'split | term')
+end, { desc = 'Open terminal at 30% height' })
+
+-- Optionally, create a keymap for easy access
+vim.keymap.set('n', '<leader>tt', '<cmd>Term30<CR>', { desc = '[T]erminal at 30% height' })
 -- TIP: Disable arrow keys in normal mode
 -- vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
 -- vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
